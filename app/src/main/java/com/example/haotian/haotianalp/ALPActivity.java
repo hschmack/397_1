f54382f9e7d7b224b75ec84cfc375875b211a397
Erwin Pang <erwinkpang@gmail.com>
2015-09-11 17:10:55 -0400

    Added coordinate loggin

--- app/src/main/java/com/example/haotian/haotianalp/ALPActivity.java
+++ app/src/main/java/com/example/haotian/haotianalp/ALPActivity.java
@@ -102,6 +102,31 @@ public class ALPActivity extends Activity {
 
     }
 
+    public boolean onTouchEvent(MotionEvent event){
+
+        int action = event.getActionMasked();
+
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN) :
+                Log.d("DEBUG_TAG","Action was DOWN " + String.valueOf(event.getX()) + ", " + String.valueOf(event.getY()));
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                Log.d("DEBUG_TAG","Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP) :
+                Log.d("DEBUG_TAG","Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL) :
+                Log.d("DEBUG_TAG","Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE) :
+                Log.d("DEBUG_TAG","Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
+    }
     @Override
     protected void onResume()
     {
